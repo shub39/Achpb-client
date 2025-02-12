d13d7568b79f722cb89ff8f9b4422ca3d1a673c8
Shubham Gorai <cptnshubham39@gmail.com>
2025-02-12 11:45:34 +0530

    Initial commit

---
+++ composeApp/src/wasmJsMain/kotlin/com/kmp/template/main.kt
@@ -0,0 +1,13 @@
+package com.kmp.template
+
+import androidx.compose.material3.Text
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import kotlinx.browser.document
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport(document.body!!) {
+        Text("Hello")
+    }
+}
